92bd0e02c4c1e0afe98a1f5794d6a2b02b0e9812
Joshua S <92652314+J-Saun@users.noreply.github.com>
2022-07-09 19:53:31 -0700

    added projects jobs and contact to home page

--- modules/header.jsx
+++ modules/header.jsx
@@ -1,16 +1,45 @@
 export default function Header() {
   return (
-    <header className="py-6">
-      <div className="container">
-       <div>Joshua Saunders</div>
-       <div>
-        <a href="#" className="" id="">home</a>
-        <a href="#projects" className="" id="">projects</a>
-        <a href="#resources" className="" id="">resources</a>
-        <a href="#contact" className="" id="">contact</a>
-       </div>
-
+    <div className="container mt-16 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full h-fit">
+      <div className="flex flex-wrap md:flex-nowrap">
+        <nav className="inline-block  lg:mr-24 lg:w-4 fixed left-percentage  xl:block">
+          <div className="">
+          {/* <div className="absolute left-50 transform -translate-x-1/2 space-y-6 mt-36"> */}
+            <a
+              href="#"
+              className="nav-dot selected-circle block w-7 h-7 rounded-full border-4 border-nav bg-body"
+            >
+              <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">
+                Home
+              </span>
+            </a>
+            <a
+              href="#"
+              className="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body"
+            >
+              <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">
+                Home
+              </span>
+            </a>
+            <a
+              href="#"
+              className="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body"
+            >
+              <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">
+                Home
+              </span>
+            </a>
+            <a
+              href="#"
+              className="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body"
+            >
+              <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">
+                Home
+              </span>
+            </a>
+          </div>
+        </nav>
       </div>
-    </header>
+    </div>
   );
 }
--- modules/nav/navbar.jsx
+++ modules/nav/navbar.jsx
@@ -3,14 +3,34 @@ import NavItem from "../../components/Link/nav_link";
 
 export default function Navbar() {
   return (
-    <nav class="">
-      <a href={'/'}>Joshua Saunders</a>
-      {/* <ul class=""> */}
-        <NavItem content={"Home"} href={"#home"} />
-        <NavItem content={"Projects"} href={"#projects"} />
-        <NavItem content={"Resources"} href={"#resources"} />
-        <NavItem content={"Contact"} href={"#contact"} />
-      {/* </ul> */}
+    <nav className="">
+     <div className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+            <div className="text-lg font-bold">Joshua Saunders</div>
+            <div className="hidden md:flex space-x-12 items-center">
+              <a href="#" className="text-selected-text">
+                Home
+              </a>
+              <a href="#work">My Projects</a>
+              <a href="#resources">Resources</a>
+              <a href="#hire">
+                <button className="px-6 py-2 bg-theme font-bold">Contact Me</button>
+              </a>
+            </div>
+            <div className="md:hidden">
+              <svg
+                width="26"
+                height="18"
+                viewBox="0 0 26 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z"
+                  fill="white"
+                />
+              </svg>
+            </div>
+          </div>
     </nav>
   );
 }
